ffbf39bfa7f8884610dd1e37e2d2d789f67184b9
Tarsis <br.tmvdl@gmail.com>
2023-10-08 21:11:03 -0300

    Removing useless files

--- sample/index.js
+++
@@ -1,9 +0,0 @@
-const { Database } = require('@brtmvdl/database')
-
-const db = new Database(process.env.DATA_PATH)
-
-const users = db.in('users')
-
-const user = users.new()
-
-user.writeMany({ email: 'mail@mail.com', password: 'password' })
